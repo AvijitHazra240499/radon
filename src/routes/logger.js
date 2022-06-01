eceb98ad8ed3d01da564c99e28c81785933caf43
Your Name <you@example.com>
2022-06-01 16:02:56 +0530

    adding

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+const endpoint="https://www.functionup.org"
+const log=function(){
+    console.log("I am inside the log function")
+}
+module.exports.endpoint=endpoint
+module.exports.batch=this.batch
+module.exports.log=log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const externalmodule= require ('./logger')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+   console.log("The constant in logger route has a value " +externalmodule.endpoint )
+   console.log('The current batch'+externalmodule.batch)
+  externalmodule.log()
+   res.send('My first ever api!')
+});
+
+router.get('/test-me1', function (req, res) {
+    res.send('My first & foremost ever api!')
+});
+
+router.get('/test-me2', function (req, res) {
+    res.send('My second ever api!')
+});
+
+router.get('/test-me3', function (req, res) {
+    res.send('My last ever api!')
 });
 
 module.exports = router;
