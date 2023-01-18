4166272d25be3d36f9ff7c6fde591511d269a2f2
Avijit Hazra <avijithazrabwn@gmail.com>
2023-01-19 00:30:40 +0530

    final push

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Axios  from 'axios'
+
+const App = () => {
+    const [value,updateValue]=useState("")
+    const [visible,setVisible]=useState("")
+
+    async function urlshort(){
+        const host="https://url-shortner-y5ky.onrender.com"
+      const Longurl=await Axios.post(`${host}/url/shorten`, {
+       longUrl:value
+      })
+      let Shorturl=`${host}/${Longurl.data.data.urlCode}`
+
+      setVisible(Shorturl)
+    }
+  return (
+    <div>
+      <input onChange={(e)=>{
+       updateValue(e.target.value)
+      }}/>
+      <button onClick={()=>{urlshort()}}>
+        Submit
+      </button>
+      
+       <a href={visible}>
+        <span>{visible}</span>
+      </a>
+      
+    </div>
+  )
+}
+
+
+
+
+
+
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import reactDom from "react-dom/client"
+import App from "./App"
+
+
+const _root=reactDom.createRoot(document.getElementById("root"))
+_root.render(
+   <App/> 
+)
